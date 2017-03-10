5f2fa932ca616da320ee27f98bdd4030da3c72c2
Oliver Joseph Ash <oliverjash@gmail.com>
2017-03-10 12:58:03 +0000

    Initial commit

---
+++ src/entry.js
@@ -0,0 +1,5 @@
+import SortableSimple from './index'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+ReactDOM.render(React.createElement(SortableSimple), window.root)
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    entry: './src/entry.js',
+    output: {
+        filename: './target/entry.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: [
+                    ],
+                    plugins: [
+                        'babel-plugin-transform-react-jsx',
+                        'babel-plugin-transform-decorators-legacy',
+                        'babel-plugin-transform-class-properties',
+                        'babel-plugin-transform-object-rest-spread',
+                    ],
+                }
+            }
+        ],
+    },
+}
